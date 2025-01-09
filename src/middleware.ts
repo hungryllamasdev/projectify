73079f13b313d3f4f33437a3d49e4c284d05cc78
syedhamzasiff <syedhamzasiff@gmail.com>
2025-01-09 06:02:19 +0500

    add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import { auth } from "./auth";
+
+const protectedRoutes = [""];
+
+export default async function middleware(request: NextRequest) {
+    const session = await auth()
+    const {pathname} = request.nextUrl
+
+    const isProtected = protectedRoutes.some((route) => pathname.startsWith(route));
+
+    if (isProtected && !session) {
+        return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ['']
+}
