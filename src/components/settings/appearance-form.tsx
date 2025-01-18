c0590996688384cc4765e6af2f1f6533cb2c4295
Syed Ali Rizwan <alirizwan2003@gmail.com>
2025-01-18 14:41:40 +0500

    add dark mode

--- src/components/settings/appearance-form.tsx
+++ src/components/settings/appearance-form.tsx
@@ -17,6 +17,7 @@ import {
     FormMessage,
 } from "@/components/ui/form";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { useTheme } from "next-themes";
 import { toast } from "@/hooks/use-toast";
 
 const appearanceFormSchema = z.object({
@@ -37,18 +38,22 @@ const defaultValues: Partial<AppearanceFormValues> = {
 };
 
 export function AppearanceForm() {
+    const { setTheme } = useTheme();
+
     const form = useForm<AppearanceFormValues>({
         resolver: zodResolver(appearanceFormSchema),
         defaultValues,
     });
 
     function onSubmit(data: AppearanceFormValues) {
+        setTheme(data.theme);
         toast({
             title: "You submitted the following values:",
             description: (
                 <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
                     <code className="text-white">
                         {JSON.stringify(data, null, 2)}
+                        {JSON.stringify(data.theme)}
                     </code>
                 </pre>
             ),
@@ -167,7 +172,9 @@ export function AppearanceForm() {
                     )}
                 />
 
-                <Button type="submit">Update preferences</Button>
+                <Button type="submit" className="">
+                    Update preferences
+                </Button>
             </form>
         </Form>
     );
--- src/components/settings/sidebar-nav.tsx
+++ src/components/settings/sidebar-nav.tsx
@@ -21,7 +21,7 @@ export function SidebarNav({ className, items, ...props }: SidebarNavProps) {
             className={cn(
                 "flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1",
                 className
-            )}
+            )} 
             {...props}
         >
             {items.map((item) => (
